10c2269ea03ea0cc29017e20f8f8a70ef74713de
Aum Pauskar <aumpauskar111@gmail.com>
2024-05-14 22:54:11 +0530

    `config.json` now handles arguments based on hrs mins sec

--- main.py
+++ main.py
@@ -6,14 +6,35 @@ import json
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
     delay = int(config["delay"]) - 5
-    duration = int(config["duration"]) * 60
+    duration = config["duration"]
+    try:
+        specified_time, format = duration.split(" ")
+        if format == "day":
+            duration = int(specified_time) * 86400
+        elif format == "hr":
+            duration = int(specified_time) * 3600
+        elif format == "min":
+            duration = int(specified_time) * 60
+        elif format == "sec":
+            duration = int(specified_time)
+    except ValueError:
+        print("Please specify the time in the following format (config.json)")
+        print('''
+              day: for 1day or multiple days
+              hr: for 1hour or multiple hours
+              min: for 1minute or multiple minutes
+              sec: for 1second or multiple seconds
+              ''')
+        exit()
+    # duration = int(config["duration"]) * 60
 
 pic_name = 1
 
 def take_photo():
     global pic_name
-    os.system(f"libcamera-still --nopreview -o timelapse/timelapse{pic_name}.jpg --vflip")
+    os.system(f"libcamera-still --nopreview -o timelapse2/timelapse{pic_name}.jpg --vflip")
     pic_name += 1
+    print(pic_name,"picture taken suncessfully")
 
 def timelapse(delay, duration):
     start_time = time.time()
@@ -22,6 +43,7 @@ def timelapse(delay, duration):
     while time.time() < end_time:
         take_photo()
         time.sleep(delay)
+        print(start_time - end_time, "time remaining")
 
 def main():
     timelapse(delay, duration)
